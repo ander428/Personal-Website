4d86458745572e40cf3264e722fcb5d0f3c36f09
ander428 <ander428@mail.chapman.edu>
2019-06-26 15:01:38 -0700

    update all components to use hooks

--- react/portfolio-website/src/components/Gallery/Gallery.js
+++ react/portfolio-website/src/components/Gallery/Gallery.js
@@ -2,26 +2,16 @@ import React, { Component } from "react";
 import Panel from "./Panel";
 import "./Gallery.css";
 
-class Gallery extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      data: props.data
-    };
-  }
-
-  render() {
-    return (
-      <div className={"accordion"} style={{ filter: "grayscale(33%)" }}>
-        <ul>
-          {this.state.data.map((item, i) => (
-            <Panel {...item} key={i} />
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+const Gallery = props => {
+  return (
+    <div className={"accordion"} style={{ filter: "grayscale(33%)" }}>
+      <ul>
+        {props.data.map((item, i) => (
+          <Panel {...item} key={i} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Gallery;
--- react/portfolio-website/src/components/Gallery/Panel.js
+++ react/portfolio-website/src/components/Gallery/Panel.js
@@ -1,33 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Gallery.css";
 
-class Panel extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-
-    this.toggle = this.toggle.bind(this);
-  }
-
-  toggle() {
-    this.setState(prevState => ({
-      open: !prevState.open
-    }));
-  }
-
-  render() {
-    return (
-      <li style={{ backgroundImage: `url(${this.props.image})` }}>
-        <div>
-          <a href={this.props.link}>
-            <h2>{this.props.name}</h2>
-            <p>{this.props.description}</p>
-          </a>
-        </div>
-      </li>
-    );
-  }
-}
+const Panel = props => {
+  return (
+    <li style={{ backgroundImage: `url(${this.props.image})` }}>
+      <div>
+        <a href={this.props.link}>
+          <h2>{this.props.name}</h2>
+          <p>{this.props.description}</p>
+        </a>
+      </div>
+    </li>
+  );
+};
 
 export default Panel;
