4a4c70a970659ad0d27c4243e45d2b061f6aa9e7
ander428 <ander428@mail.chapman.edu>
2019-12-23 14:58:59 -0800

    delete pld nav bar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { HashRouter as Router } from "react-router-dom";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 import { defaultTheme } from "./utils/Themes/Default";
 import Routes from "./components/Routes/Routes";
-import NavBar from "./components/NavBar/NavBar";
 import NavTabs from "./components/NavTabs/NavTabs";
 
 class App extends Component {
