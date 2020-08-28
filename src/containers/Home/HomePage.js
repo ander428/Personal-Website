7b6617a727253206f075935ccee2cf08179ce641
ander428 <ander428@mail.chapman.edu>
2020-08-27 19:16:20 -0700

    css fixes

--- src/containers/Home/HomePage.js
+++ src/containers/Home/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styles from "./HomePage.styles.js";
 import { withStyles } from "@material-ui/core";
 import Header from "../../components/Header/Header";
