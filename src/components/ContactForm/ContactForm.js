af3141a136f6b61a16d04d8091f2d5243d78096b
ander428 <ander428@mail.chapman.edu>
2020-09-06 21:13:33 -0700

    fix warnings pt2

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -20,46 +20,46 @@ const ContactForm = props => {
     //     name: "Joshua Anderson",
     //     feedback: "test"
     // }
-    var service_id = "default_service";
-    var template_id = "template_YYrpbC3s";
+    // var service_id = "default_service";
+    // var template_id = "template_YYrpbC3s";
     // const receiverEmail = constants.CONTACT[2].text;
     // const user = "jwanderson198@gmail.com"
 
-    const handleFirstName = (e) => {
-        setFirstName(e.target.value)
-    }
+    // const handleFirstName = (e) => {
+    //     setFirstName(e.target.value)
+    // }
 
-    const handleLastName = (e) => {
-        setLastName(e.target.value)
-    }
+    // const handleLastName = (e) => {
+    //     setLastName(e.target.value)
+    // }
 
-    const handleEmail = (e) => {
-        setEmail(e.target.value)
-    }
+    // const handleEmail = (e) => {
+    //     setEmail(e.target.value)
+    // }
 
-    const handleMessage = (e) => {
-        setMessage(e.target.value)
-    }
+    // const handleMessage = (e) => {
+    //     setMessage(e.target.value)
+    // }
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+    // const handleSubmit = (e) => {
+    //     e.preventDefault()
 
-        var template_params = {
-            "reply_to": "reply_to_value",
-            "from_name": "from_name_value",
-            "to_name": "to_name_value",
-            "message_html": "message_html_value"
-        }
+    //     var template_params = {
+    //         "reply_to": "reply_to_value",
+    //         "from_name": "from_name_value",
+    //         "to_name": "to_name_value",
+    //         "message_html": "message_html_value"
+    //     }
 
-        emailjs.send(
-            service_id,
-            template_id,
-            templateParams
-        ).then(result => {
-            setSent(true)
-        }).catch(err => {
-            console.log('Failed to send feedback. Error: ', err)
-        })
+        // emailjs.send(
+        //     service_id,
+        //     template_id,
+        //     templateParams
+        // ).then(result => {
+        //     setSent(true)
+        // }).catch(err => {
+        //     console.log('Failed to send feedback. Error: ', err)
+        // })
     }
     
     return(
@@ -67,19 +67,19 @@ const ContactForm = props => {
             <h2 className={classes.text}>CONTACT</h2>
             <h6 className={classes.text}>Please fill out this form to contact me via email.</h6>
             <div className="d-flex flex-column justify-content-center"></div>
-            <form className={classes.form} onSubmit={handleSubmit}>
+            <form className={classes.form} onSubmit={}>
                 <div className="p-2 col-example text-left" >
                     <TextField
                         className={classes.textInput}
                         label={"First Name"}
                         variant={"outlined"}
-                        onChange={handleFirstName}
+                        onChange={}
                         style={{ width: "50%" }} />
                     <TextField
                         className={classes.textInput}
                         label={"Last Name"}
                         variant={"outlined"}
-                        onChange={handleLastName}
+                        onChange={}
                         style={{ width: "50%" }} />
                 </div>
                 <div className={"p-2 col-example text-left"}>
@@ -87,7 +87,7 @@ const ContactForm = props => {
                         className={classes.textInput} 
                         label={"Email"} 
                         variant={"outlined"}
-                        onChange={handleEmail} />
+                        onChange={} />
                 </div>
                 <div className={"p-2 col-example text-left"}>
                     <TextField 
@@ -96,7 +96,7 @@ const ContactForm = props => {
                         rows={5} 
                         label={"Message"} 
                         variant={"outlined"}
-                        onChange={handleMessage} />
+                        onChange={} />
                 </div>
                 <div className={"p-2 col-example text-center"}>
                     <Button className={classes.button} type={"submit"}>Send</Button>
