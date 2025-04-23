99617aaa6f6f9de300e63375a0cadd505fddbc93
Joshua Anderson <ander428@mail.chapman.edu>
2025-04-22 20:28:55 -0400

    Update PublicationsPage.js

--- src/containers/Publications/PublicationsPage.js
+++ src/containers/Publications/PublicationsPage.js
@@ -48,7 +48,7 @@ const PublicationsPage = props => {
               <div className={classes.rightPanel}>
                 <NavTabs tabValue={tabValue}/>
                 <div className={classes.info}>
-                  <p className={classes.section}>Publications (Updated {constants.UPDATED}):</p>
+                  <p className={classes.section}>Publications (Updated {constants.GLOBAL_VARS.updated}):</p>
                   <div ref={aRef} />
                   <div>
                     <br />
